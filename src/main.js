65e720176c0d34d940d20ff7eda888b36602ff45
AA <angelangelov97@gmail.com>
2020-07-11 02:42:23 +0300

    sentry changes

--- src/main.js
+++ src/main.js
@@ -25,22 +25,39 @@ subscriptionStore.init();
 
 Vue.config.productionTip = false
 
+let ignoreErrors = [	
+	/.*ObjectMultiplex.*/gm,	
+	/.*Invalid JSON RPC.*/gm,	
+	/.*MaxListenersExceededWarning.*/gm,	
+	/.*Heads up!.*/gm,	
+	/.*invalid number value.*/gm,	
+	/.*Out of Gas.*/gm,	
+	/.*invalid address.*/gm,	
+	/.*property '_address' of undefined.*/gm,	
+	/.*TypeError: Cannot read property 'chart' of undefined.*/gm,	
+	/.*Cannot read property '0' of undefined.*/gm,
+   	/.*Non-Error promise rejection captured with keys.*/gm,
+]
 //adding Sentry as soon as possible
 // process.env.NODE_ENV == 'production' && Sentry.init({ 	
-//   dsn: 'https://5494f535e0244513a301f2912f5d899f@sentry.io/4169463',	
-//   ignoreErrors: [	
-//     /.*ObjectMultiplex.*/gm,	
-//     /.*Invalid JSON RPC.*/gm,	
-//     /.*MaxListenersExceededWarning.*/gm,	
-//     /.*Heads up!.*/gm,	
-//     /.*invalid number value.*/gm,	
-//     /.*Out of Gas.*/gm,	
-//     /.*invalid address.*/gm,	
-//     /.*property '_address' of undefined.*/gm,	
-//     /.*TypeError: Cannot read property 'chart' of undefined.*/gm,	
-//     /.*Cannot read property '0' of undefined.*/gm,
-//	   /.*Non-Error promise rejection captured with keys.*/gm,
-//   ],	
+//   dsn: 'https://5494f535e0244513a301f2912f5d899f@sentry.io/4169463',
+// 	 beforeSend(event, hint) {
+// 		let error = hint.originalException
+// 		console.log(error, event, "ERROR EVENT")
+// 		if(error && error.message) {
+// 			let matches = ignoreErrors.some(regex => regex.test(error))
+// 			if(matches.length) {
+// 				console.log("MATCH ERROR IGNORE")
+// 				event.fingerprint = ['not-tracked']
+// 			}
+// 		}
+// 		if(ignoreErrors.some(regex => regex.test(event.value))) {
+// 			console.log("MATCH ERROR IGNORE")
+// 			event.fingerprint = ['not-tracked']
+// 		}
+// 		return event
+// 	 },
+//   ignoreErrors: ignoreErrors,	
 //   integrations: [	
 //     new CaptureConsole({	
 //       levels: ['error', 'debug', 'assert']	
