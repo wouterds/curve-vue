74ec215c93b075776efb69a9537bedeba640773a
AA <angelangelov97@gmail.com>
2020-08-02 22:47:06 +0300

    fix swap exchange rates

--- src/components/swap/Swap.vue
+++ src/components/swap/Swap.vue
@@ -276,11 +276,11 @@
                 if(val == this.to_currency) {
                     this.to_currency = oldval;
                 }
-                this.swapExchangeRate()
+                //this.swapExchangeRate()
                 this.from_cur_handler()
             },
             to_currency(val, oldval) {
-                this.swapExchangeRate()
+                //this.swapExchangeRate()
                 this.to_cur_handler()
             },
             swapwrapped() {
